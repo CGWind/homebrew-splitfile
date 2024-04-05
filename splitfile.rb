bfd5185305e65276eec4d43531d65092ffca70c0
mr.xx <xin_chen-neu@reachauto.com>
2024-04-05 16:25:35 +0800

    #change---splitfile1.0.0

--- splitfile.rb
+++ splitfile.rb
@@ -1,7 +1,7 @@
 class Splitfile < Formula
   desc "Split large diff files into smaller chunks"
   homepage "https://github.com/CGWind/homebrew-splitfile"
-  url "https://github.com/CGWind/homebrew-splitfile/raw/master/splitfile_0.0.1.tar.gz"
+  url "https://github.com/CGWind/homebrew-splitfile/raw/master/splitfile_1.0.0.tar.gz"
   sha256 "d619fde963492db76ad7b56c0f8b55bb2694e850fd9782ea460968dd8d298dbd"
   license ""
 
